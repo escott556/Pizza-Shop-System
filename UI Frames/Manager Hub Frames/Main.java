5b935be9710a3987ad0b30a4d32c69d0afb7e3f6
Madeline Severance <114512597+MFSeverance@users.noreply.github.com>
2023-02-11 11:27:27 -0500

    Add files via upload
    Needs to be renamed, contains code the login frame and logout functionality.

--- UI Frames/Manager Hub Frames/Main.java
+++ UI Frames/Manager Hub Frames/Main.java
@@ -1,25 +1,38 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	//stage for use in the change scene method
+	private static Stage stg;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Manager Frame");
+			stg = primaryStage;
+			primaryStage.setResizable(false);
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
+			primaryStage.setTitle("Pizza Shop Accounting System");
+			primaryStage.setScene(new Scene(root, 600, 400));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//change scene method
+	public void changeScene(String fxml) throws IOException {
+		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+		stg.getScene().setRoot(pane);
 	}
 	
 	public static void main(String[] args) {
