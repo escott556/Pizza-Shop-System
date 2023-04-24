5db61ff297aa4f297beb96e7fe6ac5fecb931a13
connor0mathis <122981883+connor0mathis@users.noreply.github.com>
2023-04-24 14:43:12 -0400

    Update PizzaShopSystemController.java
    Updated code of graph section

--- PizzaShopSystemController.java
+++ PizzaShopSystemController.java
@@ -734,121 +734,104 @@ public class Controller implements Initializable {
 	/* All Graph data entries below */
 	@FXML
 	public void GrandTotal(ActionEvent actionEvent) { // Grand total graph data
-		Axis<String> xAxis = lineChart.getXAxis();
-		xAxis.setLabel("Month");
-
-		Axis<Number> yAxis = lineChart.getYAxis();
-		yAxis.setLabel("Revenue");
-
-		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); // Create the series for data to
-		series.setName("GrandTotal"); // be implemented.
-
+		
+		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); //Create a series for this data to be implemented.
+		
+		series.setName("Grand Total"); //Sets the name for the series for it to display in the legend
+		
 		// Full data series for the GrandTotal type
-		series.getData().add(new XYChart.Data<>("Jan", 45));
-		series.getData().add(new XYChart.Data<>("Feb", 78));
-		series.getData().add(new XYChart.Data<>("Mar", 95));
-		series.getData().add(new XYChart.Data<>("Apr", 105));
-		series.getData().add(new XYChart.Data<>("May", 111));
-		series.getData().add(new XYChart.Data<>("Jun", 205));
-		series.getData().add(new XYChart.Data<>("Jul", 305));
-		series.getData().add(new XYChart.Data<>("Aug", 406));
-		series.getData().add(new XYChart.Data<>("Sep", 555));
-		series.getData().add(new XYChart.Data<>("Oct", 599));
-		series.getData().add(new XYChart.Data<>("Nov", 690));
-		series.getData().add(new XYChart.Data<>("Dec", 890));
-
-		// adding series to lineChart
+		series.getData().add(new XYChart.Data<>("Jan", 0));
+		series.getData().add(new XYChart.Data<>("Feb", 20));
+		series.getData().add(new XYChart.Data<>("Mar", 40));
+		series.getData().add(new XYChart.Data<>("Apr", 60));
+		series.getData().add(new XYChart.Data<>("May", 80));
+		series.getData().add(new XYChart.Data<>("Jun", 100));
+		series.getData().add(new XYChart.Data<>("Jul", 120));
+		series.getData().add(new XYChart.Data<>("Aug", 140));
+		series.getData().add(new XYChart.Data<>("Sep", 160));
+		series.getData().add(new XYChart.Data<>("Oct", 180));
+		series.getData().add(new XYChart.Data<>("Nov", 200));
+		series.getData().add(new XYChart.Data<>("Dec", 220));
+		
+		//adding series to lineChart
 		lineChart.getData().add(series);
 
 	}
 
 	@FXML
 	public void InHouse(ActionEvent actionEvent) { // In-house graph data
-		Axis<String> xAxis = lineChart.getXAxis();
-		xAxis.setLabel("Month");
-
-		Axis<Number> yAxis = lineChart.getYAxis();
-		yAxis.setLabel("Revenue");
-
-		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); // Create the series for data to
-																						// be implemented.
-		series.setName("InHouse");
+		
+		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); //Create a series for this data to be implemented.
+		
+		series.setName("In-house"); //Sets the name for the series for it to display in the legend
+		
 		// Full data series for the InHouse type
-		series.getData().add(new XYChart.Data<>("Jan", 10));
-		series.getData().add(new XYChart.Data<>("Feb", 30));
+		series.getData().add(new XYChart.Data<>("Jan", 0));
+		series.getData().add(new XYChart.Data<>("Feb", 60));
 		series.getData().add(new XYChart.Data<>("Mar", 40));
 		series.getData().add(new XYChart.Data<>("Apr", 120));
-		series.getData().add(new XYChart.Data<>("May", 300));
+		series.getData().add(new XYChart.Data<>("May", 814));
 		series.getData().add(new XYChart.Data<>("Jun", 450));
-		series.getData().add(new XYChart.Data<>("Jul", 513));
+		series.getData().add(new XYChart.Data<>("Jul", 700));
 		series.getData().add(new XYChart.Data<>("Aug", 230));
-		series.getData().add(new XYChart.Data<>("Sep", 150));
-		series.getData().add(new XYChart.Data<>("Oct", 719));
-		series.getData().add(new XYChart.Data<>("Nov", 800));
-		series.getData().add(new XYChart.Data<>("Dec", 815));
-
-		// adding series to lineChart
+		series.getData().add(new XYChart.Data<>("Sep", 5));
+		series.getData().add(new XYChart.Data<>("Oct", 809));
+		series.getData().add(new XYChart.Data<>("Nov", 546));
+		series.getData().add(new XYChart.Data<>("Dec", 375));
+		
+		//adding series to lineChart
 		lineChart.getData().add(series);
 
 	}
 
 	@FXML
 	public void Takeout(ActionEvent actionEvent) { // Take-out graph data
-		Axis<String> xAxis = lineChart.getXAxis();
-		xAxis.setLabel("Month");
-
-		Axis<Number> yAxis = lineChart.getYAxis();
-		yAxis.setLabel("Revenue");
-
-		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); // Create the series for data to
-		series.setName("Takeout"); // be implemented.
-
+		
+		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); //Create a series for this data to be implemented.
+		
+		series.setName("Takeout"); //Sets the name for the series for it to display in the legend
+		
 		// Full data series for the Takeout type
-		series.getData().add(new XYChart.Data<>("Jan", 5));
-		series.getData().add(new XYChart.Data<>("Feb", 50));
-		series.getData().add(new XYChart.Data<>("Mar", 100));
-		series.getData().add(new XYChart.Data<>("Apr", 150));
-		series.getData().add(new XYChart.Data<>("May", 100));
+		series.getData().add(new XYChart.Data<>("Jan", 0));
+		series.getData().add(new XYChart.Data<>("Feb", 20));
+		series.getData().add(new XYChart.Data<>("Mar", 40));
+		series.getData().add(new XYChart.Data<>("Apr", 60));
+		series.getData().add(new XYChart.Data<>("May", 80));
 		series.getData().add(new XYChart.Data<>("Jun", 100));
 		series.getData().add(new XYChart.Data<>("Jul", 120));
 		series.getData().add(new XYChart.Data<>("Aug", 140));
-		series.getData().add(new XYChart.Data<>("Sep", 150));
-		series.getData().add(new XYChart.Data<>("Oct", 119));
-		series.getData().add(new XYChart.Data<>("Nov", 500));
+		series.getData().add(new XYChart.Data<>("Sep", 160));
+		series.getData().add(new XYChart.Data<>("Oct", 180));
+		series.getData().add(new XYChart.Data<>("Nov", 200));
 		series.getData().add(new XYChart.Data<>("Dec", 220));
-
-		// adding series to lineChart
+		
+		//adding series to lineChart
 		lineChart.getData().add(series);
 
 	}
 
 	@FXML
 	public void Delivery(ActionEvent actionEvent) { // Delivery graph data
-		Axis<String> xAxis = lineChart.getXAxis();
-		xAxis.setLabel("Month");
-
-		Axis<Number> yAxis = lineChart.getYAxis();
-		yAxis.setLabel("Revenue");
-
-		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); // Create the series for data to
-																						// be implemented.
-		series.setName("Delivery");
-
+		
+		XYChart.Series<String, Number> series = new XYChart.Series<String, Number>(); //Create a series for this data to be implemented.
+		
+		series.setName("Delivery"); //Sets the name for the series for it to display in the legend
+		
 		// Full data series for the Delivery type
-		series.getData().add(new XYChart.Data<>("Jan", 30));
-		series.getData().add(new XYChart.Data<>("Feb", 100));
-		series.getData().add(new XYChart.Data<>("Mar", 150));
-		series.getData().add(new XYChart.Data<>("Apr", 160));
-		series.getData().add(new XYChart.Data<>("May", 180));
-		series.getData().add(new XYChart.Data<>("Jun", 190));
+		series.getData().add(new XYChart.Data<>("Jan", 0));
+		series.getData().add(new XYChart.Data<>("Feb", 20));
+		series.getData().add(new XYChart.Data<>("Mar", 40));
+		series.getData().add(new XYChart.Data<>("Apr", 60));
+		series.getData().add(new XYChart.Data<>("May", 80));
+		series.getData().add(new XYChart.Data<>("Jun", 100));
 		series.getData().add(new XYChart.Data<>("Jul", 120));
 		series.getData().add(new XYChart.Data<>("Aug", 140));
-		series.getData().add(new XYChart.Data<>("Sep", 200));
-		series.getData().add(new XYChart.Data<>("Oct", 348));
-		series.getData().add(new XYChart.Data<>("Nov", 340));
-		series.getData().add(new XYChart.Data<>("Dec", 223));
-
-		// adding series to lineChart
+		series.getData().add(new XYChart.Data<>("Sep", 160));
+		series.getData().add(new XYChart.Data<>("Oct", 180));
+		series.getData().add(new XYChart.Data<>("Nov", 200));
+		series.getData().add(new XYChart.Data<>("Dec", 220));
+		
+		//adding series to lineChart
 		lineChart.getData().add(series);
 
 	}
