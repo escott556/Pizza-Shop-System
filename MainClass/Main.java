3fc451b5e1dcb8ceda553db735dbe451ef2a3dd4
Bhakti Patel <115588802+Bhakti3001@users.noreply.github.com>
2023-04-25 10:29:57 -0400

    Update Main.java

--- MainClass/Main.java
+++ MainClass/Main.java
@@ -40,7 +40,7 @@ public class Main extends Application {
 	
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		launch(args);
 		Test_Calculations.testCalculateCost();
 		Test_Calculations.testCalculateSubTotal();
