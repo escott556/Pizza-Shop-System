498837ceb6fa32851ab01b5f42e14e714224a33a
Bhakti Patel <115588802+Bhakti3001@users.noreply.github.com>
2023-04-25 10:05:09 -0400

    Create Main.java

---
+++ MainClass/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+
+	// stage for use in the change scene method
+	private static Stage stg;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			stg = primaryStage;
+
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			primaryStage.setTitle("Pizza Shop Accounting System");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Method for changing scene
+	 * 
+	 * @param fxml
+	 * @throws IOException
+	 */
+	public void changeScene(String fxml) throws IOException {
+		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+		stg.getScene().setRoot(pane);
+	
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
